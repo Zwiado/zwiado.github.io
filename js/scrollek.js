c93f38d122a620a5a78f7c996f53f429e5d8fc47
Zwiado <72385649+Zwiado@users.noreply.github.com>
2022-03-09 12:46:58 +0100

    Update scrollek.js

--- js/scrollek.js
+++ js/scrollek.js
@@ -1,8 +1,3 @@
-$('.atomic-button').on('click', function() {
-    $('nav').toggleClass('active');
-    $('html').toggleClass('scroll');
-})
- 
 $('button').on('click', function () {
          $('body,html').animate({
             scrollTop: $('.onas').offset().top
